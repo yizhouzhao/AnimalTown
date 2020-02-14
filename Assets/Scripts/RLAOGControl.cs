466452b1a7777640b296c6606761cc044d492e36
yizhouzhao <yizhou_zhao@berkeley.edu>
2020-02-14 14:38:46 -0800

    add code for AOG control

--- Assets/Scripts/RLAOGControl.cs
+++ Assets/Scripts/RLAOGControl.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CharacterState {
+    public float positionX;
+    public float positionZ;
+    public int sceneCode;
+    public int holdObjectCode;
+    public int meetCharacterCode;
+}
+
 public class RLAOGControl : MonoBehaviour
 {
     [Header("character")]
     public AnimalCharacter animalCharacter;
+    public CharacterState characterState;
 
 
     [Header("Task Sequence")]
@@ -16,10 +25,21 @@ public class RLAOGControl : MonoBehaviour
 
     private bool canTakeAction = false;
 
+    [Header("Task Sequence")]
+    public List<float> beliefList;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(ExecuteAfterTime(1f));
+        
+        //init belief
+        beliefList = new List<float>();
+        for(int i = 0; i < 3; i++)
+        {
+            beliefList.Add(UnityEngine.Random.Range(0f, 1f));
+        }
+
         IEnumerator ExecuteAfterTime(float time)
         {
             yield return new WaitForSeconds(time);
@@ -32,8 +52,23 @@ public class RLAOGControl : MonoBehaviour
 
             // Code to execute after the delay
             canTakeAction = true;
+
+            //init 
+            characterState = GetCharacterState();
         }
 
+    }
+
+    public CharacterState GetCharacterState()
+    {
+        CharacterState character_state = new CharacterState();
+        character_state.positionX = this.transform.position.x;
+        character_state.positionZ = this.transform.position.z;
+        character_state.sceneCode = GetSceneToolCode();
+        character_state.holdObjectCode = GetHoldObjectCode();
+        character_state.meetCharacterCode = GetMeetCharacterCode();
+
+        return character_state;
     }
 
     // Update is called once per frame
@@ -48,9 +83,12 @@ public class RLAOGControl : MonoBehaviour
                 if (currentTask == ETaskType.Walk)
                 {
                     GameObject targetObject = GameObject.Find(taskDesription[currentTaskIndex]);
-                    animalCharacter.navControl.TravelTo(targetObject.transform.position);
+                    Vector3 targetLocation = new Vector3(targetObject.transform.position.x, this.transform.position.y, targetObject.transform.position.z);
+                    animalCharacter.navControl.TravelTo(targetLocation);
 
-                    if (Vector3.Distance(targetObject.transform.position, transform.position) < 2f) //done traveling
+                    if (Vector3.Distance(targetLocation, transform.position) < 2f || 
+                        (animalCharacter.sceneTool != null && animalCharacter.sceneTool.gameObject.name == taskDesription[currentTaskIndex])
+                        || animalCharacter.meetPickupObject != null && animalCharacter.meetPickupObject.gameObject.name == taskDesription[currentTaskIndex]) //done traveling
                     {
                      
                         currentTaskIndex++;
@@ -60,6 +98,11 @@ public class RLAOGControl : MonoBehaviour
                 {
                     animalCharacter.ActWithSceneTool();
                     animalCharacter.ActWithAnimalCharacter();
+                    currentTaskIndex++;
+                }
+                else if(currentTask == ETaskType.PickupDrop)
+                {
+                    animalCharacter.PickupDropObject();
                     currentTaskIndex++;
                 }
                 else if (currentTask == ETaskType.Use)
@@ -85,7 +128,6 @@ public class RLAOGControl : MonoBehaviour
             default:
                 {
                     return 0;
-                    throw new NotImplementedException();
                 }
         }
     }
@@ -111,7 +153,6 @@ public class RLAOGControl : MonoBehaviour
             default:
                 {
                     return 0;
-                    throw new NotImplementedException();
                 }
         }
     }
@@ -122,5 +163,13 @@ public class RLAOGControl : MonoBehaviour
             return 0;
         else
             return 1;
+    }
+
+    public void Explore(int state_action_code, ETaskType taskType, string TaskDescription)
+    {
+        if(state_action_code == 0) //Scene
+        {
+            
+        }
     }
 }

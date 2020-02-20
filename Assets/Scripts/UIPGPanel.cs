926744521711ff11dd056beab08ab13102776ca5
yizhouzhao <yizhou_zhao@berkeley.edu>
2020-02-20 15:59:18 -0800

    add PGpanel

---
+++ Assets/Scripts/UIPGPanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPGPanel : MonoBehaviour
+{
+    public Text timeText;
+    public Text activitText;
+
+    public Slider healthSlide;
+    public Slider energySlide;
+    public Slider moneySlide;
+
+    public void SetContentFromPGNode(PGNode pgNode)
+    {
+        float pgTime = pgNode.pgTime;
+        int hour = (int)pgTime / 3600;
+        int min = (int)pgTime % 3600 / 60;
+        int sec = (int)pgTime % 60;
+
+        timeText.text = hour.ToString("00") + ":" + min.ToString("00") + ":" + sec.ToString("00");
+        activitText.text = pgNode.activityType.ToString();
+        healthSlide.value = pgNode.fullness;
+        energySlide.value = pgNode.energy;
+        moneySlide.value = pgNode.money;
+    }
+}
